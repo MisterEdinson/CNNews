6894920873f27107ff66f0e5a5867a01a12bcc7c
Edyard Karastoyka <88448179+MisterEdinson@users.noreply.github.com>
2023-06-02 19:50:04 +0500

    add recycleview
    add recycle
    add adapter
    add glide image lib

--- app/src/main/java/com/example/cnnews/ui/home/MainActivity.kt
+++ app/src/main/java/com/example/cnnews/ui/home/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.example.cnnews.ui.home
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cnnews.R
+import com.example.cnnews.data.network.model.ArticlesItem
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private val viewModel: HomeViewModel by viewModels()
+    private var newsAdapter: HomeNewsAdapter? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initAdapter()
+        viewModel.dataList.observe(this) {
+            it.articles.let { list ->
+                newsAdapter?.setList(list as List<ArticlesItem>)
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        newsAdapter = HomeNewsAdapter()
+        rvNewsHome.apply {
+            adapter = newsAdapter
+        }
     }
 }
 
